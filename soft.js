acf9662f2fac714b9391ee40401145616fb84229
Prazual Karki <prazualkarki@gmail.com>
2022-11-07 21:38:23 +0545

    initial commit

---
+++ soft.js
@@ -0,0 +1,3 @@
+var username = prompt("can you write your name plz..");
+var course=prompt("welcome" + username ",which courses do you want to study:");
+alert("Oh..I can guess you are intelligent as you are choosing " + course);
